40a219dfd64f8bc8aa9661b8870738b24a2f6005
Pieter Colpaert <pieter@irail.be>
2010-12-09 20:32:48 +0100

    Cleaned up rewrite branch to start a total rewrite of the API without a mobile client that interferes

--- index.php
+++ index.php
@@ -1,62 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>iRail API</title>
+  </head>
+  <body>
+      <h1>Welcome developer</h1>
+      <p>Welcome to the iRail API. If you want to implement train (more to come later) schedules in your application you are at the right place.</p>
+      <h1>Usage</h1>
+      <p><a href="http://project.irail.be/cgi-bin/trac.fcgi/wiki/APIv1">here</a></p>
+      <h1>Grab the code</h1>
+      <p>The code of this website is free and open source. It is licensed under the GPL v3 and you can find it at Github: </p>
+      <p><a href="http://github.com/iRail/">Github.com/iRail</a></p>
+      <h1>Authors</h1>
+      <p><a href="http://project.irail.be">Project iRail</a>
+      <p>Copyright &copy; 2008, 2009, 2010, 2011 Yeri Tiete ("<a href="http://yeri.be" target="_blank">Tuinslak</a>") - yeri@irail.be,</p>
+      <p>Copyright &copy; 2010, 2011 Pieter Colpaert (<a href="http://bonsansnom.wordpress.com" target="_blank">http://bonsansnom.wordpress.com</a>) - pieter@irail.be,</p>
+      <p>And <a href="http://project.irail.be/cgi-bin/trac.fcgi/wiki/Contributors">others</a>.</p>
+      <h1>Blog</h1>
+      <a href="http://blog.irail.be">Blog.iRail.be</a>
+      <h1>Bugs</h1>
+      <p>Nobody is perfect. Writing a webscraper is not the most fault tolerant system you would ever write. Since we think sooner or later NMBS will release an API of their own, we didn't mind writing clean code. So before you dive in, don't mind the mess.</p>
+      <p>On the other hand if you noticed a flaw, something that doesn't work, something that might be important, ... feel free to report a new ticket at the <a href="http://project.irail.be/cgi-bin/trac.fcgi/newticket">project iRail</a> site.</p>
+      <p><br/><i> Thanks for your interest and happy hacking :-)</i><br/><br/>
+          <i>Some rights reserved - CC By-Sa - <img src="http://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-sa.png" width="88" height="33" alt="Creative Commons Attribution, Share alike"/></i>
+      </p>
+  </body>
 <?php
-/**
- *  Copyright 2008, 2009, 2010 Yeri "Tuinslak" Tiete (http://yeri.be), and others
-    Copyright 2010 Pieter Colpaert (pieter@irail.be - http://bonsansnom.wordpress.com)
-
-	This file is part of iRail.
-
-    iRail is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    iRail is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with iRail.  If not, see <http://www.gnu.org/licenses/>.
-
-	http://project.irail.be - http://irail.be
-
-	source available at http://github.com/Tuinslak/iRail
-
-
- * @author pieterc
- */
-
-include("Page.php");
-include_once("api/InputHandlers/StationsInput.php");
-
-class Main extends Page {
-
-    function __construct() {
-        $this-> page = array(
-            "title" => "iRail.be",
-            "subtitle" => "Mobile"
-        );
-        $stationsinput = new StationsInput();
-        $this->page["stationarray"] = $stationsinput->generate_js_array(parent::newRequestInstance());
-        
-        $this->page["date"] = date("D d/m/Y H:i");
-        if(isset($_COOKIE["from"]))
-            $this->page["from"] = $_COOKIE["from"];
-        else
-            $this->page["from"] = "";
-
-        if(isset($_COOKIE["to"]))
-            $this->page["to"] = $_COOKIE["to"];
-        else
-            $this->page["to"] = "";
-
-    }
-
-}
-
-//__MAIN__
-
-$page = new Main();
-$page -> setDetectLanguageAndTemplate(true);
-$page -> buildPage("Main.tpl");
+// GA stats; try to include it in most files please
+include '../includes/googleAnalytics.php';
 ?>
+</html>
